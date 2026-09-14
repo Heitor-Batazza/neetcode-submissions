5c2b422106d9b99c3337caf1b853e417213e8a37
Heitor-Batazza <heitorbatazzaoliveira@gmail.com>
2026-09-14 09:52:40 -0300

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        index = 0
+        value = nums[0]
+        sum = 0
+        count = 1
+
+        while index + value < len(nums) - 1:
+            for i in range(index + 1, index + 1 + nums[index]):
+                if nums[i] + i >= sum:
+                    index = i
+                    value = nums[i]
+                    sum = nums[i] + i
+            count += 1
+
+
+        return count
+
+
+        
